b692a8a270ba3e28cfcfb8c2e17cc6385a62122b
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 16:13:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,17 @@
 # Write your code here.
+
 def line(array) 
   if array.length == 0 
     puts "The line is currently empty."
-  else 
-    message = "The line is currently:"
+ end 
+ 
+ 
+ # else 
+  #  message = "The line is currently:"
     
-  array.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
-  end 
-  puts "#{message}"
-  end 
-end 
+  #array.each_with_index do |value, index| 
+  #  message += " #{index.to_i+1}. #{value}"
+  #end 
+  #puts "#{message}"
+  #end 
+#end 
