fdda743c9de4721dd4ac7dd04c26f02dd4885e52
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 16:03:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,5 @@
 # Write your code here.
 
 def line(katz_deli)
-  if line > 0 
-    Puts katz_deli
-  else
-   Puts "The line is currently empty."
-  end 
+return  "The line is currently empty."
 end
